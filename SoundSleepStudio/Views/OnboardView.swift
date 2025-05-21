b69f688aaa668a39903b0d3732ff1c0346690446
Wentao <s3956080@student.rmit.edu.au>
2025-05-21 09:04:54 +0800

    Onboarding page

---
+++ SoundSleepStudio/Views/OnboardView.swift
@@ -0,0 +1,84 @@
+//
+//  OnboardView.swift
+//  SoundSleepStudio
+//
+//  Created by Wentao Guo on 21/05/25.
+//
+
+import SwiftUI
+
+struct OnboardView: View {
+    var body: some View {
+        ZStack {
+        //MARK: Backgroud
+            Color(
+                .sRGB, red: 20 / 255, green: 0 / 255, blue: 0 / 255, opacity: 1
+            )
+            .ignoresSafeArea()
+
+            VStack(spacing: 30) {
+                Spacer()
+                //MARK: IMAGE
+                Image("sleepCloud")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 180, height: 180)
+
+            
+                //MARK: TITLE
+                Text("Welcome to\nSound Sleep")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+
+                //MARK: BODY
+                VStack(alignment: .leading, spacing: 25) {
+                    HStack(alignment: .top) {
+                 
+                        Image(systemName: "music.note")
+                            .foregroundColor(.purple)
+                            .font(.title3)
+                        Text("Sync gentle music with your heartbeat")
+                            .foregroundColor(.white)
+                   
+                    }
+
+                    HStack(alignment: .top) {
+                     
+                        Image(systemName: "bed.double.fill")
+                            .foregroundColor(.purple)
+                            .font(.title3)
+                        Text(
+                            "Gradually slowing the tempo to lull you into sleep"
+                        )
+                        .foregroundColor(.white)
+                        
+                    }
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal, 40)
+
+                Spacer()
+
+                //MARK: BUTTON
+                Button(action: {
+         
+                }) {
+                    Text("Continue")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.purple)
+                        .cornerRadius(16)
+                }
+                .padding(.horizontal, 40)
+                .padding(.bottom, 40)
+            }
+        }
+    }
+}
+
+#Preview {
+    OnboardView()
+}
